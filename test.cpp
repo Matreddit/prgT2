7794812e1dbaf9a326effaca538d0d4f9d5bf1f3
Matreddit <82979311+Matreddit@users.noreply.github.com>
2024-05-19 21:58:58 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -17,6 +17,19 @@ void bubbleSort(int * arr, int size){
     }
 }
 
+void shellSort(int *arr, int n){
+    for (int gap = n/2; gap > 0; gap /= 2){
+        for (int i = gap; i < n; i += 1){
+            int temp = arr[i];
+            int j;
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap)
+                arr[j] = arr[j - gap];
+            
+            arr[j] = temp;
+        }
+    }
+}
+
 
 
 int main() {
@@ -45,7 +58,7 @@ int main() {
     cout << "---------\n";
 
 
-    bubbleSort(array, N);
+    shellSort(array, N);
 
     cout << "---------\n";
     for(int i = 0; i < N; i++){
