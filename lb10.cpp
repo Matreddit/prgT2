bfc85fc4cc523c239ad1a97b44e7f12f6731322b
Matreddit <82979311+Matreddit@users.noreply.github.com>
2024-05-20 21:30:24 +0300

    Update lb10.cpp

--- lb10.cpp
+++ lb10.cpp
@@ -12,27 +12,69 @@ using namespace std;
 struct List
 {
     int inf;
-    List* next;
+    List* link;
 };
 
 
+void addToEnd(List *& Head, int inf){
+    List *p = new List;
+    p->inf = inf;
+    p->link = NULL;
+    if(!Head){
+        Head = p;
+    } else{
+        List *TmpHead = Head;
+        while(TmpHead->link){
+            TmpHead = TmpHead->link;
+        } 
+        TmpHead->link = p;
+    }
+
+}
+
+List *findLast(List * node){
+    while(node->link)
+        node = node->link;
+    return node;
+}
+
+void printList(List * Head){
+    while(Head){
+        cout << Head->inf << " ";
+        Head = Head->link;
+    }
+    cout << endl;
+}
+
+
+// 5.a
+bool doesSorted12(List*head){
+
+}
+
 int main(){
 
-    List *Head = NULL, *Tail = NULL;
+    List *Head = NULL; // створили поводок щоб не загубить список
 
-    List *p = new List;
-    p->inf = 3;
-    p->next = NULL;
-
-    Head = p;
-    Tail = p;
-
-    delete p;
-
+    for(int i = 0; i < 50; i++){
+        addToEnd(Head, i);
+    }
     
 
+    printList(Head);
+
+    cout << "\n\n\n";
+    if(doesSorted12(Head)) cout << "true\n";
+    else cout << "false\n";
+
+
     
-    
+    // deletin
+    while(Head){
+        List *pD = Head;
+        Head = Head->link;
+        delete pD;
+    }
     
     return 0;
 }
