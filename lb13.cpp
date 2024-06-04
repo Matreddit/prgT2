eff0d5a74219e813f9b647c412c044ca1db91aeb
Matreddit <82979311+Matreddit@users.noreply.github.com>
2024-06-04 23:04:41 +0300

    Update lb13.cpp

--- lb13.cpp
+++ lb13.cpp
@@ -11,28 +11,54 @@ struct Tree
     int data;
     Tree *left, *right;
 
-    /*
+    
     // геніальний метод ініціалізацї
     Tree(int value){
         data = value;
         left = right = NULL;
     }
-    */
+    
 };
 
 
 
-void addLeftBranch(Tree *& T, int inf){
-    Tree *Branch = new Tree;
-    Branch->data = inf;
-    Branch->left=NULL; Branch->right=NULL;
-    if(!T)
-        T = Branch;
-    else{
-        Tree *qT = T;
-        while(qT->left)
-            qT = qT->left;
-        qT->left = Branch;
+// void addLeftBranch(Tree *& T, int inf){
+//     Tree *Branch = new Tree;
+//     Branch->data = inf;
+//     Branch->left=NULL; Branch->right=NULL;
+//     if(!T)
+//         T = Branch;
+//     else{
+//         Tree *qT = T;
+//         while(qT->left)
+//             qT = qT->left;
+//         qT->left = Branch;
+//     }
+// }
+
+// Функція для прямого обходу дерева (in-order traversal)
+void inOrder(Tree* root) {
+    if (root == NULL) {
+        return;
+    }
+    inOrder(root->left);
+    cout << root->data << " ";
+    inOrder(root->right);
+}
+
+void printTree(Tree* root, string indent = "", bool isLeft = true) {
+    if (root != NULL) {
+        cout << indent;
+        if (isLeft) {
+            cout << "L---";
+            indent += "|   ";
+        } else {
+            cout << "R---";
+            indent += "    ";
+        }
+        cout << root->data << endl;
+        printTree(root->left, indent, true);
+        printTree(root->right, indent, false);
     }
 }
 
@@ -40,12 +66,21 @@ int main(){
 
     Tree *T = NULL;
 
-    addLeftBranch(T, 3);
-    addLeftBranch(T, 14);
-    addLeftBranch(T, 88);
+    T = new Tree(0);
+    T->left = new Tree(-1);
+    T->left->left = new Tree(-4);
 
+    T->left->right = new Tree(-2);
+    
+    T->right = new Tree(1);
+    T->right->right = new Tree(4);
+    T->right->left = new Tree(2);
 
+    
 
+    printTree(T);
+
+    cout << "\n\n\n------\n\n";
     Tree *zxc = T;
     while (zxc)
     {
@@ -53,6 +88,7 @@ int main(){
         zxc = zxc->left;
     }
     
+
 
 
 
